c249ad659750624a8d740c06e0bd60f36aaf018e
Clinton <80900245+pravton@users.noreply.github.com>
2023-07-26 11:53:23 -0400

    basic api calls

---
+++ src/services/CallService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/';
+
+async function apiCall(method, url, data) {
+  try {
+    const response = await axios({ method, url: `${BASE_URL}/${url}`, data });
+    return response.data;
+  } catch (error) {
+    console.error(`Error occurred while making ${method} request to ${url}: ${error}`);
+    throw error;
+  }
+}
+
+export async function getCalls() {
+  return apiCall('get', 'activities');
+}
+
+export async function getCall(id) {
+  return apiCall('get', `activities/${id}`);
+}
+
+export async function updateCall(id, data) {
+  return apiCall('patch', `activities/${id}`, data);
+}
+
+export async function resetCalls() {
+  return apiCall('patch', 'reset');
+}
