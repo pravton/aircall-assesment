44bd95661290db04aa2b4d49b06512634005b113
Clinton <80900245+pravton@users.noreply.github.com>
2023-07-27 17:41:23 -0400

    build test gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { CallProvider } from './context/CallContext';
 import ActivityFeed from './components/ActivityFeed';
 import ActivityDetail from './components/ActivityDetail';
